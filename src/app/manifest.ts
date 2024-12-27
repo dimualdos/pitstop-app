497c4c1be123a18f5b9555eebf4b5918b5f79119
Dmitry Pyanzov <dimualdos@gmail.com>
2024-12-27 23:18:22 +0400

    manifest.ts

---
+++ src/app/manifest.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: 'PitStop',
+		short_name: 'PS',
+		start_url: '/source=pwa',
+		scope: '/',
+		description: 'Кузовной ремонт в Москве',
+		display: 'standalone',
+		orientation: 'portrait',
+		theme_color: '#1b2e43',
+		background_color: '#ccc6c2',
+
+		icons: [
+			{ src: '/touch-icons/192x192.png', sizes: '192x192', type: 'image/png' },
+			{ src: '/touch-icons/192x192-mask.png', sizes: '192x192', type: 'image/png' },
+			{ src: '/touch-icons/512x512.png', sizes: '512x512', type: 'image/png' },
+			{ src: '/touch-icons/512x512-mask.png', sizes: '512x512', type: 'image/png' }
+		]
+	}
+}
