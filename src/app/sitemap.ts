02d6767fd0ae7073b001dbe74abd1190fe571bee
Dmitry Pyanzov <dimualdos@gmail.com>
2025-01-07 15:04:11 +0400

    fix bags

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,86 +1,86 @@
-import { MetadataRoute } from "next";
+import { type MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://pitstop-online.ru",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/body-repair",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/bumper-repair",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/bumper-replacement",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/car-painting",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/car-door-painting",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/car-fender-painting",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/bumper-painting",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/painting-the-hood",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/car-roof-painting",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/local-painting",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/body-polishing",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/headlight-polishing",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/polishing-scratches",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/dent-removal-PDR",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-    {
-      url: "https://pitstop-online.ru/contacts",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-    },
-  ];
+	return [
+		{
+			url: 'https://pitstop-online.ru',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/body-repair',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/bumper-repair',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/bumper-replacement',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/car-painting',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/car-door-painting',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/car-fender-painting',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/bumper-painting',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/painting-the-hood',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/car-roof-painting',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/local-painting',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/body-polishing',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/headlight-polishing',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/polishing-scratches',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/dent-removal-PDR',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		},
+		{
+			url: 'https://pitstop-online.ru/contacts',
+			lastModified: new Date(),
+			changeFrequency: 'weekly'
+		}
+	]
 }
